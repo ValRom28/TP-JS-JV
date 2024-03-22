56096ed9eeea04c6f58d96aaa1f5529b847cc5e0
ValRom <valentin.romanet@etu.univ-orleans.fr>
2024-03-22 10:53:02 +0100

    fix : routes

--- js/app.js
+++ js/app.js
@@ -1,7 +1,5 @@
 import Home from './views/pages/Home.js';
-import ArticleAll from './views/pages/ArticleAll.js';
-import ArticleShow from './views/pages/ArticleShow.js';
-import About from './views/pages/About.js';
+import Items from './views/pages/Items.js';
 import Error404 from './views/pages/Error404.js';
 
 import Utils from './services/Utils.js';
@@ -9,9 +7,7 @@ import Utils from './services/Utils.js';
 // List of supported routes. Any url other than these routes will throw a 404 error
 const routes = {
     '/'                     : Home
-    , '/about'              : About
-    , '/articles'           : ArticleAll
-    , '/articles/:id'       : ArticleShow
+    , '/items'           : Items
 };
 
 // The router code. Takes a URL, checks against the list of supported routes and then renders the corresponding content page.
@@ -29,6 +25,7 @@ const router = async () => {
     // If the parsed URL is not in our list of supported routes, select the 404 page instead
     let page = routes[parsedURL] ? new routes[parsedURL] : Error404
     
+    console.log(page)
     content.innerHTML = await page.render();
 }
 
