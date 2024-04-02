392eb44056440bb82c5c076e7674d6613c73564d
Valentin Romanet <valentin.romanet@orange.fr>
2024-04-02 12:29:55 +0200

    feat : début localstorage

--- js/views/pages/PokePage.js
+++ js/views/pages/PokePage.js
@@ -7,6 +7,7 @@ import EquipeProvider from "../../services/EquipeProvider.js";
 export default class PokePage {
     constructor() {
         this.notation = 0;
+        this.favorites = JSON.parse(localStorage.getItem('favorites')) || [];
     }
 
     async render() {
@@ -32,9 +33,10 @@ export default class PokePage {
         
         return /*html*/`
             <div class="container">
-           
+            
             <main>
                 <p> N°${pokemon.id} </p>
+                <span class="fav" id="fav">★</span>
                 <h1> ${pokemon.name["french"]} </h1>
                 <ul>
                 <li> <p> ${pokemon.name["english"]} </p> </li>
@@ -60,56 +62,71 @@ export default class PokePage {
             </main>
 
             <aside>
-            <h2> Types </h2>
-            <ul>
-            ${pokemon.type.map(type => `<li> <p class="${type}"> ${type} </p> </li>`).join('')}
-            </ul>
-            <h2> Stats </h2>
-            <ul>
-            <li>
-            <p> HP : ${pokemon.base["HP"]} </p>
-            <div class="progress-bar ${getColorClass(pokemon.base["HP"])}">
-            <div class="progress-bar-value" style="width: ${pokemon.base["HP"] / 2}%"></div>
-            </div>
-             </li>
-            <li>
-            <p> Attack : ${pokemon.base["Attack"]} </p>
-            <div class="progress-bar ${getColorClass(pokemon.base["Attack"])}">
-            <div class="progress-bar-value" style="width: ${pokemon.base["Attack"] / 2}%"></div>
-            </div>
-             </li>
-            <li>
-            <p> Defense : ${pokemon.base["Defense"]} </p>
-            <div class="progress-bar ${getColorClass(pokemon.base["Defense"])}">
-            <div class="progress-bar-value" style="width: ${pokemon.base["Defense"] / 2}%"></div>
-            </div>
-             </li>
-            <li>
-            <p> Sp. Attack : ${pokemon.base["Sp. Attack"]} </p>
-            <div class="progress-bar ${getColorClass(pokemon.base["Sp. Attack"])}">
-            <div class="progress-bar-value" style="width: ${pokemon.base["Sp. Attack"] / 2}%"></div>
-            </div>
-             </li>
-            <li>
-            <p> Sp. Defense : ${pokemon.base["Sp. Defense"]} </p>
-            <div class="progress-bar ${getColorClass(pokemon.base["Sp. Defense"])}">
-            <div class="progress-bar-value" style="width: ${pokemon.base["Sp. Defense"] / 2}%"></div>
-            </div>
-             </li>
-            <li>
-            <p> Speed : ${pokemon.base["Speed"]} </p>
-            <div class="progress-bar ${getColorClass(pokemon.base["Speed"])}">
-            <div class="progress-bar-value" style="width: ${pokemon.base["Speed"] / 2}%"></div>
-            </div>
-             </li>
-            
-
-
-            </ul>
-        </aside>
-
-            </div>
+                <h2> Types </h2>
+                <ul>
+                    ${pokemon.type.map(type => `<li> <p class="${type}"> ${type} </p> </li>`).join('')}
+                </ul>
+                <h2> Stats </h2>
+                <ul>
+                    <li>
+                        <p> HP : ${pokemon.base["HP"]} </p>
+                        <div class="progress-bar ${getColorClass(pokemon.base["HP"])}">
+                            <div class="progress-bar-value" style="width: ${pokemon.base["HP"] / 2}%"></div>
+                        </div>
+                    </li>
+                    <li>
+                        <p> Attack : ${pokemon.base["Attack"]} </p>
+                        <div class="progress-bar ${getColorClass(pokemon.base["Attack"])}">
+                            <div class="progress-bar-value" style="width: ${pokemon.base["Attack"] / 2}%"></div>
+                        </div>
+                    </li>
+                    <li>
+                        <p> Defense : ${pokemon.base["Defense"]} </p>
+                        <div class="progress-bar ${getColorClass(pokemon.base["Defense"])}">
+                            <div class="progress-bar-value" style="width: ${pokemon.base["Defense"] / 2}%"></div>
+                        </div>
+                    </li>
+                    <li>
+                        <p> Sp. Attack : ${pokemon.base["Sp. Attack"]} </p>
+                        <div class="progress-bar ${getColorClass(pokemon.base["Sp. Attack"])}">
+                            <div class="progress-bar-value" style="width: ${pokemon.base["Sp. Attack"] / 2}%"></div>
+                        </div>
+                    </li>
+                    <li>
+                        <p> Sp. Defense : ${pokemon.base["Sp. Defense"]} </p>
+                        <div class="progress-bar ${getColorClass(pokemon.base["Sp. Defense"])}">
+                            <div class="progress-bar-value" style="width: ${pokemon.base["Sp. Defense"] / 2}%"></div>
+                        </div>
+                    </li>
+                    <li>
+                        <p> Speed : ${pokemon.base["Speed"]} </p>
+                        <div class="progress-bar ${getColorClass(pokemon.base["Speed"])}">
+                            <div class="progress-bar-value" style="width: ${pokemon.base["Speed"] / 2}%"></div>
+                        </div>
+                    </li>
+                </ul>
+            </aside>
+        </div>
         `;
+    }
+
+    addToFavorites(pokemonId) {
+        if (!this.favorites.includes(pokemonId)) {
+            this.favorites.push(pokemonId);
+            localStorage.setItem('favorites', JSON.stringify(this.favorites));
+        }
+    }
+
+    removeFromFavorites(pokemonId) {
+        const index = this.favorites.indexOf(pokemonId);
+        if (index !== -1) {
+            this.favorites.splice(index, 1);
+            localStorage.setItem('favorites', JSON.stringify(this.favorites));
+        }
+    }
+
+    isFavorite(pokemonId) {
+        return this.favorites.includes(pokemonId);
     }
 
     renderStars() {
@@ -184,6 +201,17 @@ export default class PokePage {
                 console.log("Ajouté à l'équipe");
             });
         }
+        document.querySelector('.fav').addEventListener('click', () => {
+            const request = Utils.parseRequestURL();
+            console.log(request.id);
+            if (this.isFavorite(request.id)) {
+                this.removeFromFavorites(request.id);
+                document.getElementById('fav').classList.remove('filled');
+            } else {
+                this.addToFavorites(request.id);
+                document.getElementById('fav').classList.add('filled');
+            }
+        });
     }
     
 }
